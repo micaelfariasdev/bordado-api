ec80d7d19384c68d364d5fd430023e5e59526cba
Micael Farias <micaelfarias.dev@gmail.com>
2025-11-13 22:46:15 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -20,7 +20,8 @@ const SECRET = process.env.JWT_SECRET
 const app = express()
 const port = 3000
 
-app.use(cors('*'))
+app.use(cors({ origin: '*' }))
+
 
 app.use(express.json())
 app.use("/api/pedidos", pedidoRoutes)
