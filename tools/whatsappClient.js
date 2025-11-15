49371dd550162ae30566a9bfff6dc591b330f494
Micael Farias <micaelfarias.dev@gmail.com>
2025-11-15 12:22:47 -0300

    add rota do chromium ao new client

--- tools/whatsappClient.js
+++ tools/whatsappClient.js
@@ -15,6 +15,7 @@ export async function startClient(userId) {
       dataPath: path.resolve('./sessions'),
     }),
     puppeteer: {
+      executablePath: '/usr/bin/chromium-browser',
       headless: true,
       args: [
         '--no-sandbox',
